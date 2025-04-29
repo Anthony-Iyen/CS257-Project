2152b593fcfdc6768f1e3a07e3cb257207d66be2
Anthony-Iyen <anthonyiyen15@gmail.com>
2025-04-29 14:02:34 -0500

    Modifed CSV_bug_detect and created a fixed csv of animals

--- data/CSV_Bug_Detect.py
+++ data/CSV_Bug_Detect.py
@@ -3,10 +3,11 @@ import sys
 
 
 filename = sys.argv[1]
+writer = csv.writer(sys.stdout)
 with open(filename) as f:
     reader = csv.reader(f)
     for row in reader:
         if row[1] != "Animalia":
-            print(row[0])
+            writer.writerow(row)
 
 
